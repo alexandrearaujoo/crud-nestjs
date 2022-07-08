f6851a5d7e26951ec14f0170167c85f498b7660b
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-07 21:48:13 -0300

    feat: moongose schema done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://alexandre:alexandre013@crud-nestjs.e4nojjv.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
